590e9465fa5f3d33ed0632748f9c79c05c74aa4a
Mike Vanis <hello@mikevanis.com>
2022-03-01 16:33:26 +0000

    esp-now and mqtt simultaneously

--- moth-net-hub/moth-net-hub.ino
+++ moth-net-hub/moth-net-hub.ino
@@ -1,42 +1,57 @@
 #include <esp_now.h>
 #include <WiFiManager.h>
+#include <PubSubClient.h>
 #include <Preferences.h>
+
+#define LED 2
 
 Preferences preferences;
 
+WiFiManager wm;
+
 uint8_t globalMac[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-char rfidStr[18];
-typedef struct message_struct {
-  uint8_t rfid[4];
-} message_struct;
 
-message_struct msg;
+enum sensorTypes {
+  cap_touch,
+  simple_switch,
+  cam_movement,
+  radar,
+  cam_photo
+};
 
-String aio_key;
+//Structure example to send data
+typedef struct struct_message {
+  byte ID;
+  sensorTypes sensors;
+  int eventVal;
+} struct_message;
+
+struct_message msg;
+
+String aio_key, aio_user;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+boolean readyToSend = false;
+String nextFeed, nextValue;
 
 void setup() {
 
   // start the serial connection
   Serial.begin(115200);
   pinMode(0, INPUT_PULLUP);
+  pinMode(2, OUTPUT);
+  digitalWrite(LED, LOW);
 
   // Setup ESP Now
-  WiFi.mode(WIFI_STA);
-  if (esp_now_init() == ESP_OK) {
-    Serial.println("ESPNow Init Success");
-  }
-  else {
-    Serial.println("ESPNow Init Failed");
-    //ESP.restart();
-  }
-  esp_now_register_recv_cb(onDataReceive);
-
-
+  WiFi.mode(WIFI_AP_STA);
 
   // WiFi manager
-  WiFiManager wm;
+  WiFiManagerParameter aio_user_value("aio_user", "Adafruit IO username", "", 40);
   WiFiManagerParameter aio_key_value("aio_key", "Adafruit IO key", "", 40);
   wm.addParameter(&aio_key_value);
+  wm.addParameter(&aio_user_value);
 
   if (digitalRead(0) == LOW) {
     wm.startConfigPortal("moth-net", "badgersandfoxes");
@@ -45,9 +60,17 @@ void setup() {
     wm.autoConnect("moth-net", "badgersandfoxes");
   }
 
-  Serial.println("Connected to WiFi.");
+  int ch = WiFi.channel();
+  Serial.print("Channel: ");
+  Serial.println(ch);
+  esp_wifi_set_channel(ch, WIFI_SECOND_CHAN_NONE);
 
-  Serial.println(strlen(aio_key_value.getValue()));
+  if (strlen(aio_user_value.getValue()) > 0) {
+    Serial.println("Saving custom fields");
+    preferences.begin("moth-net", false);
+    preferences.putString("aio-user", aio_user_value.getValue());
+    preferences.end();
+  }
   if (strlen(aio_key_value.getValue()) > 0) {
     Serial.println("Saving custom fields");
     preferences.begin("moth-net", false);
@@ -58,68 +81,73 @@ void setup() {
   Serial.println("Loading preferences...");
   preferences.begin("moth-net", false);
   aio_key = preferences.getString("aio-key", "");
+  aio_user = preferences.getString("aio-user", "");
   preferences.end();
 
   Serial.print("AIO key: ");
   Serial.println(aio_key);
-  while (1);
-}
+  Serial.print("AIO user: ");
+  Serial.println(aio_user);
 
-double lat = 42.331427;
-double lon = -83.045754;
-double ele = 0;
-bool toSend = false;
-char rfid[18];
+  connectToMqtt();
+
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    //ESP.restart();
+  }
+  esp_now_register_recv_cb(onDataReceive);
+}
 
 void loop() {
-
-  // io.run(); is required for all sketches.
-  // it should always be present at the top of your loop
-  // function. it keeps the client connected to
-  // io.adafruit.com, and processes any incoming data.
-//  if (toSend) {
-//    toSend = false;
-//
-//    // connect to io.adafruit.com
-//    Serial.print("Connecting to Adafruit IO");
-//    io.connect();
-//
-//    // wait for a connection
-//    while (io.status() < AIO_CONNECTED) {
-//      Serial.print(".");
-//      delay(500);
-//    }
-//
-//    // we are connected
-//    Serial.println();
-//    Serial.println(io.statusText());
-//
-//    freader1->save(rfidStr, lat, lon, ele);
-//
-//    delay(4000);
-//    WiFi.mode(WIFI_STA);
-//    // WiFi.disconnect();
-//    if (esp_now_init() == ESP_OK) {
-//      Serial.println("ESPNow Init Success");
-//    }
-//    else {
-//      Serial.println("ESPNow Init Failed");
-//      ESP.restart();
-//    }
-//    io.run();
-//
-//  }
-
-  //  String tag = "01D5F3B2";
-  //  // return if the value hasn't changed
-  //
-  //  // save the current state to the analog feed
-  //  Serial.print("sending -> ");
-  //  Serial.println(tag);
-  //  freader1->save(tag, lat, lon, ele);
-  //
-  delay(10);
+  client.loop();
 }
+
+void connectToMqtt() {
+  // Connect to WiFi
+  //wm.autoConnect("moth-net", "badgersandfoxes");
+  //Serial.println("Connected to WiFi.");
+  // Connect to MQTT
+  client.setServer("io.adafruit.com", 1883);
+  client.setCallback(mqttCallback);
+  if (client.connect("moth-net-client", aio_user.c_str(), aio_key.c_str())) {
+    Serial.println("Connected to MQTT server");
+    String feedName = aio_user + String("/f/moth-hub-led");
+    client.subscribe(feedName.c_str());
+    for (int i = 0; i < 3; i++) {
+      digitalWrite(LED, HIGH);
+      delay(50);
+      digitalWrite(LED, LOW);
+      delay(50);
+    }
+  }
+  else {
+    Serial.println("Failed to connect to MQTT server!");
+    digitalWrite(LED, HIGH);
+  }
+}
+
+void disconnectFromMqtt() {
+  WiFi.mode(WIFI_STA);
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  if ((char)payload[0] == '1') {
+    digitalWrite(LED, HIGH);
+    delay(50);
+    digitalWrite(LED, LOW);
+  }
+  Serial.println();
+}
+
 // On data received
 void onDataReceive(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
   memcpy(&msg, incomingData, sizeof(msg));
@@ -131,10 +159,17 @@ void onDataReceive(const uint8_t * mac_addr, const uint8_t *incomingData, int le
   Serial.println(macStr);
   Serial.print("Bytes received: ");
   Serial.println(len);
-  Serial.print("Data: ");
+  Serial.print("Sensor type: ");
+  Serial.println(msg.sensors);
+  Serial.print("ID: ");
+  Serial.println(msg.ID);
+  Serial.print("Value: ");
+  Serial.println(msg.eventVal);
 
-  snprintf(rfidStr, sizeof(rfidStr), "%02x%02x%02x%02x", msg.rfid[0], msg.rfid[1], msg.rfid[2], msg.rfid[3]);
-  //Serial.println(rfidStr);
-  Serial.print("sending...");
-  toSend = true;
+  if (msg.sensors == cap_touch) {
+    client.publish((aio_user + String("/f/moth-net.bird-bath")).c_str(), String(msg.eventVal).c_str());
+  } 
+  else if (msg.sensors == cam_movement) {
+    client.publish((aio_user + String("/f/moth-net.camera")).c_str(), String(msg.eventVal).c_str());
+  }
 }
